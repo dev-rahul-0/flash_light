d7707b75d6029b828dea5343bef337d4b0c7c0db
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2024-03-04 20:57:53 +0530

    fixed

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -8,12 +8,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool inOn = false;
+
   @override
   Widget build(BuildContext context) {
     Color bigbuttoncolor = const Color(0xFF312C27);
     Color midbuttoncolor = const Color(0xFF484242);
     Color smallestbuttoncolor = const Color(0xFF504847);
-    bool isOn = false;
 
     return Scaffold(
       appBar: AppBar(
@@ -27,16 +28,17 @@ class _HomeScreenState extends State<HomeScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Icon(
-                (isOn)? Icons.wb_sunny : Icons.wb_sunny_outlined,
+                (inOn) ? Icons.wb_sunny : Icons.wb_sunny_outlined,
                 size: 100,
-                color: (isOn)?const Color(0xFFFF8E01): const Color(0xFF504847),
+                color:
+                    (inOn) ? const Color(0xFFFF8E01) : const Color(0xFF504847),
               ),
               const SizedBox(
                 height: 30,
               ),
-              Text(
-                'FlashLight:${(isOn)?"ON":"OFF"}',
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              const Text(
+                'FlashLight:OFF',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               const SizedBox(
                 height: 30,
@@ -46,21 +48,21 @@ class _HomeScreenState extends State<HomeScreen> {
                 children: [
                   Circlecontain(color: bigbuttoncolor, h: 300, w: 300),
                   Circlecontain(color: midbuttoncolor, h: 260, w: 260),
-                  Container(
+                  SizedBox(
                     width: 190,
                     height: 190,
                     child: ElevatedButton(
                       onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100)),
+                        backgroundColor: smallestbuttoncolor,
+                        foregroundColor: const Color(0xFFFF8E01),
+                      ),
                       child: const Icon(
                         Icons.power_settings_new,
                         size: 170,
-                      ),style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(100)
                       ),
-                      backgroundColor: smallestbuttoncolor!,
-                      foregroundColor: const Color(0xFFFF8E01),
-                    ),
                     ),
                   )
                 ],
@@ -74,7 +76,8 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class Circlecontain extends StatelessWidget {
-  Circlecontain({required this.color, required this.h, required this.w});
+  const Circlecontain(
+      {super.key, required this.color, required this.h, required this.w});
 
   final double w;
   final double h;
